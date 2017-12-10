c1c6f545b17b5b737ca96699182a910c9161eff9
kennexrazon <kennexrazon@gmail.com>
2017-12-10 13:12:20 +0800

    ProgrammingAssignment2 - caching the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Generate the inverse of the matrix
 
-## Write a short comment describing this function
-
+## makeCacheMatrix - takes a matrix x and returns a list of objects cached in memory
+##
+## usage: makeCacheMatrix(x)
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+		## store data outside the function enviroment
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+		## store the solve function as an object outside the function enviroment
+        setinv <- function(solve) i <<- solve
+        getinv <- function() i
+		## aggregate the objects for easier access
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve - takes x, a list of objects produced by makeCacheMatrix, and returns
+## the inverse of the matrix from the list x. The matrix inverse is stored in cache 
+## if it isn't computed yet. 
+##
+## usage: special_matrix <- makeCacheMatrix(x)
+##        cacheSolve(special_matrix)
+##
+## usage: cacheSolve(makeCacheMatrix(x))
+##
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## take the function for solving the matrix inverse
+		i <- x$getinv()
+		## check if the inverse of the matrix has already been solved
+        if(!is.null(i)) {
+				## inform the user that the return value is from cache
+                message("getting cached data")
+				## return the cached matrix inverse
+                return(i)
+        }
+		## compute the inverse of the matrix
+        data <- x$get()
+        i <- solve(data, ...)
+		## store the inverse of the matrix to cache
+        x$setinv(i)
+		## return the inverse of the matrix
+        i
 }
